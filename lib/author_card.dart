0721409f10b45e9f1859f369af31c614d7f67df5
Kim Jeoryl Henyr G. Franial <Franial-Ki@ndmu.local>
2024-02-26 14:27:40 +0800

    initial commit

--- lib/author_card.dart
+++ lib/author_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'fooderlich_theme.dart';
 import 'circle_image.dart';
 
-class AuthorCard extends StatelessWidget {
+class AuthorCard extends StatefulWidget {
  // 1
  final String authorName;
  final String title;
@@ -13,6 +13,13 @@ class AuthorCard extends StatelessWidget {
  required this.title,
  this.imageProvider,
  });
+
+  @override
+  State<AuthorCard> createState() => _AuthorCardState();
+}
+
+class _AuthorCardState extends State<AuthorCard> {
+bool _isFavorited = false;
  // 2
  @override
  Widget build(BuildContext context) {
@@ -26,7 +33,7 @@ return Container(
  Row(
  children: [
  CircleImage(
- imageProvider: imageProvider,
+ imageProvider: widget.imageProvider,
  imageRadius: 28,
  ),
  // 2
@@ -36,11 +43,11 @@ return Container(
  crossAxisAlignment: CrossAxisAlignment.start,
  children: [
  Text(
- authorName,
+ widget.authorName,
  style: FooderlichTheme.lightTextTheme.headline2,
  ),
  Text(
- title,
+ widget.title,
  style: FooderlichTheme.lightTextTheme.headline3,
  )
  ],
@@ -48,15 +55,19 @@ return Container(
 ],
  ),
  IconButton(
- // 4
- icon: const Icon(Icons.favorite_border),
+ // 1
+ icon: Icon(_isFavorited ? Icons.favorite : 
+Icons.favorite_border),
  iconSize: 30,
- color: Colors.grey[400],
- // 5
+ // 2
+ color: Colors.red[400],
  onPressed: () {
- const snackBar = SnackBar(content: Text('Favorite Pressed'));
- ScaffoldMessenger.of(context).showSnackBar(snackBar);
- }),
+ // 3
+ setState(() {
+ _isFavorited = !_isFavorited;
+ });
+ },
+),
 
  ],
  ),
